feb6990c41fd57fe76f63c70f3d176b740fa6501
linneajader <linnea.jader@gmail.com>
2018-11-14 23:07:21 +0100

    Added some doggos
    Added som doggos to the likedComponent. Also took the liberty to add some real facts about the different breeds.

--- src/AppState.js
+++ src/AppState.js
@@ -1,6 +1,9 @@
 import { Component } from 'react';
 import image1 from "./pictures/cute-dog-shiba.jpg";
-import image2 from "./pictures/short-nosed-golden.jpg";
+import image2 from "./pictures/Doggos/short-nosed-golden.jpg";
+import image3 from "./pictures/Doggos/tired-griffon.jpg";
+import image4 from "./pictures/Doggos/sad-labrador.jpg";
+import image5 from "./pictures/Doggos/boxer.jpg";
 
 class AppState extends Component {
     constructor(props) {
@@ -28,10 +31,34 @@ class AppState extends Component {
                     age: '3 mån',
                     breed: 'Golden Retriver',
                     gender: 'Hona',
+                    info: 'The Golden Retriever is one of the most popular dog breeds in the U.S. The breed’s friendly, tolerant attitude makes him a fabulous family pet, and his intelligence makes him a highly capable working dog. Golden Retrievers excel at retrieving game for hunters, tracking, sniffing out drugs, and as therapy and assistance dogs. They’re also natural athletes, and do well in dog sports such as agility and competitive obedience.'
+                },
+                {
+                    image: image3,
+                    name: 'Kent',
+                    age: '2 år',
+                    breed: 'Griffon',
+                    gender: 'Hane',
                     info: 'Lorem ipsum öjashdföjasd öasdjfölasd asd ölksadflkasdfj sa klö'
+                },
+                {
+                    image: image4,
+                    name: 'Bolle',
+                    age: '8 år',
+                    breed: 'Labrador',
+                    gender: 'Hane',
+                    info: 'The Labrador Retriever was bred to be both a friendly companion and a useful working dog breed. Historically, he earned his keep as a fisherman’s helper: hauling nets, fetching ropes, and retrieving fish from the chilly North Atlantic. Today’s Labrador Retriever is as good-natured and hard working as his ancestors, and he’s America’s most popular breed. These days the Lab works as a retriever for hunters, assistance dog to the handicapped, show competitor, and search and rescue dog, among other canine jobs.'
+                },
+                {
+                    image: image5,
+                    name: 'Angus',
+                    age: '3 år',
+                    breed: 'Boxer',
+                    gender: 'Hona',
+                    info: 'Boxers were originally bred to be medium-size guard dogs. Today, although they are a part of the AKC’s Working Group, they mostly find homes as loving family companions. They’ve got the muscular build of the guard dogs they were bred to be, and they can look imposing until you realize that within those sturdy bodies, they’re bursting with playful joie de vivre.'
                 }
             ],
-            likedDogs: [1]
+            likedDogs: [1, 2, 3, 4]
         };
     }
 
